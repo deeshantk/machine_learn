b164822dab26e2ab903dc7f9c1f8406f36fbad80
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2019-08-06 20:28:21 +0530

    Update main_fle.py

--- main_fle.py
+++ main_fle.py
@@ -3,3 +3,4 @@ import pandas as pd
 file_path = 'data.csv' # Your file path comes here. A sample data is also given in repository.
 data = pd.read_csv(file_path) # Reads the data and stores it in data.
 print(data.describe()) # Prints outs the summery of the data.
+print(data.columns). # This will print out the first column of the data.
