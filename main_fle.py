221633facefbb3721d4ae62238919645b9a533e3
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2019-08-06 20:24:41 +0530

    Update main_fle.py

--- main_fle.py
+++ main_fle.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
-file_path = ''
+file_path = 'data.csv' # Your file path comes here. A sample data is also given in repository.
+data = pd.read_csv(file_path) # Reads the data and stores it in data.
+print(data.describe()) # Prints outs the summery of the data.
