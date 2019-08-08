c3378cf4fa989f3d44639cf9fe8e731b65e5fbfd
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2019-08-09 01:00:59 +0530

    Update main_fle.py

--- main_fle.py
+++ main_fle.py
@@ -5,14 +5,14 @@ data = pd.read_csv(file_path) # Reads the data and stores it in data.
 print(data.describe()) # Prints outs the summery of the data.
 print(data.columns). # This will print out the first column of the data.
 
-y = data.Price
+y = data.Price # Target for prediction.
 
-data_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude'] 
+data_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude'] # Features for target prediction.
 
 x = data[data_featuers] 
 
 data_model = DecisionTreeRegressor(random_state = 1)
-data_model.fit(x,y)
+data_model.fit(x,y) # Train model.
 print('Lets make prediction for first 5 houses')
 print(X.head())
-print(data_model.predict(X.head()))
+print(data_model.predict(X.head())) # Predict the value of target according to features.
