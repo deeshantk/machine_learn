121a1ebd08055416d19d5d7c92e31b7faca6cb09
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2019-08-11 04:11:42 +0530

    Update validation.py

--- validation.py
+++ validation.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 
-~~~ This part of the code is same as before.
+'''This part of the code is same as before.
 
 
 file_path ='/Users/deeshantkotnala/Desktop/project/data.csv'
@@ -23,7 +23,7 @@ data_model.fit(X,y)
 print('Lets make prediction for first 5 houses')
 print(X.head())
 print(data_model.predict(X.head())) 
-~~~
+'''
 
 predicted_prices = data_model.predict(X) 
 print(mean_absolute_error(y, predicted_prices)) 
