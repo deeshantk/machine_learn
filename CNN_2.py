bcbf9c52734ea168b5c3d64429170d7b0474a6e2
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2019-10-13 03:00:24 +0530

    Update CNN_2.py

--- CNN_2.py
+++ CNN_2.py
@@ -2,3 +2,8 @@ import keras
 from keras.model import Sequential
 from keras.layer import Activation
 from keras.layer.core import Activation
+
+model = Sequential([
+  Dense = (16, input_shape = (1, ), Activation = 'relu'),
+  Dense = (32, Activation = 'relu'),
+  Dense = (2, Activation = 'relu')])
